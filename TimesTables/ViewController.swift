7f4c291e51365f57a22f4ac9c6fab36ee3b73f03
Adnan <aaziz@Adnans-MacBook-Pro.local>
2015-04-16 16:09:32 -0700

    times tables app. Max table of 10

--- TimesTables/ViewController.swift
+++ TimesTables/ViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func slidersMoved(sender: AnyObject) {
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(sliderValue.value * 10)
+    
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
